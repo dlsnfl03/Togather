38c2e2e962bc52dbce6cee98bf59857cd5b9a6a2
dlsnfl03 <shiota1@naver.com>
2024-02-10 21:33:42 +0900

    [중간 커밋]

---
+++ app/src/main/java/com/example/gdsc_2324_android/board_list.kt
@@ -0,0 +1,11 @@
+package com.example.gdsc_2324_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class board_list : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_board_list)
+    }
+}
