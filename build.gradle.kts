8dcd025920104ae762d6d319e88a1f38997fbb75
YU JEONG SEON <yjs000616@gmail.com>
2024-02-15 19:53:54 +0900

    [MOD]/ignore 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,6 @@
 plugins {
     id("com.android.application") version "8.0.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    // 구글 서비스에 gradle 플러그인에 대한 종속성 추가
+    id("com.google.gms.google-services") version "4.4.1" apply false
 }
